1b3b95cae9909fa5a65614ca6e0e6f67d9189268
patientwood <hardevsingh200184@gmail.com>
2026-03-26 17:04:01 -0700

    Update App.js
    for huggingface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useRef } from "react";
 import "./App.css";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const API_URL = "";
 
 function App() {
   const [file, setFile] = useState(null);
